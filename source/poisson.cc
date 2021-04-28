af84e52af2c3bbce8a91c635d0c4f245fe2890a2
Luca Heltai <luca.heltai@sissa.it>
2021-04-28 22:25:39 +0200

    Solution.

--- source/poisson.cc
+++ source/poisson.cc
@@ -19,6 +19,12 @@
  */
 #include "poisson.h"
 
+#include <deal.II/grid/grid_refinement.h>
+
+#include <deal.II/lac/sparse_direct.h>
+
+#include <deal.II/numerics/error_estimator.h>
+
 using namespace dealii;
 
 template <int dim>
@@ -46,6 +52,23 @@ Poisson<dim>::Poisson()
   add_parameter("Grid generator function", grid_generator_function);
   add_parameter("Grid generator arguments", grid_generator_arguments);
   add_parameter("Number of refinement cycles", n_refinement_cycles);
+
+  add_parameter("Estimator type",
+                estimator_type,
+                "",
+                this->prm,
+                Patterns::Selection("exact|kelly|residual"));
+
+  add_parameter("Marking strategy",
+                marking_strategy,
+                "",
+                this->prm,
+                Patterns::Selection("global|fixed_fraction|fixed_number"));
+
+  add_parameter("Coarsening and refinement factors",
+                coarsening_and_refinement_factors);
+
+  add_parameter("Use direct solver", use_direct_solver);
 
   this->prm.enter_subsection("Error table");
   error_table.add_parameters(this->prm);
@@ -102,7 +125,8 @@ template <int dim>
 void
 Poisson<dim>::refine_grid()
 {
-  triangulation.refine_global(1);
+  // Cells have been marked in the mark() method.
+  triangulation.execute_coarsening_and_refinement();
 }
 
 
@@ -146,6 +170,7 @@ Poisson<dim>::setup_system()
   system_matrix.reinit(sparsity_pattern);
   solution.reinit(dof_handler.n_dofs());
   system_rhs.reinit(dof_handler.n_dofs());
+  error_per_cell.reinit(triangulation.n_active_cells());
 }
 
 
@@ -221,13 +246,142 @@ template <int dim>
 void
 Poisson<dim>::solve()
 {
-  SolverControl            solver_control(1000, 1e-12);
-  SolverCG<Vector<double>> solver(solver_control);
-  solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+  if (use_direct_solver == true)
+    {
+      SparseDirectUMFPACK system_matrix_inverse;
+      system_matrix_inverse.initialize(system_matrix);
+      system_matrix_inverse.vmult(solution, system_rhs);
+    }
+  else
+    {
+      SolverControl            solver_control(1000, 1e-12);
+      SolverCG<Vector<double>> solver(solver_control);
+      solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+    }
   constraints.distribute(solution);
 }
 
 
+
+template <int dim>
+void
+Poisson<dim>::estimate()
+{
+  if (estimator_type == "exact")
+    {
+      error_per_cell = 0;
+      QGauss<dim> quad(fe->degree + 1);
+      VectorTools::integrate_difference(dof_handler,
+                                        solution,
+                                        exact_solution,
+                                        error_per_cell,
+                                        quad,
+                                        VectorTools::H1_seminorm);
+    }
+  else if (estimator_type == "kelly")
+    {
+      std::map<types::boundary_id, const Function<dim> *> neumann;
+      for (const auto id : neumann_ids)
+        neumann[id] = &neumann_boundary_condition;
+
+      QGauss<dim - 1> face_quad(fe->degree + 1);
+      KellyErrorEstimator<dim>::estimate(dof_handler,
+                                         face_quad,
+                                         neumann,
+                                         solution,
+                                         error_per_cell,
+                                         ComponentMask(),
+                                         &coefficient);
+    }
+  else if (estimator_type == "residual")
+    {
+      // h_T || f+\Delta u_h ||_0,T
+      // + \sum over faces
+      // 1/2 (h_F)^{1/2} || [n.\nabla u_h] ||_0,F
+
+      QGauss<dim - 1> face_quad(fe->degree + 1);
+      QGauss<dim>     quad(fe->degree + 1);
+
+      std::map<types::boundary_id, const Function<dim> *> neumann;
+      for (const auto id : neumann_ids)
+        neumann[id] = &neumann_boundary_condition;
+
+      KellyErrorEstimator<dim>::estimate(dof_handler,
+                                         face_quad,
+                                         neumann,
+                                         solution,
+                                         error_per_cell,
+                                         ComponentMask(),
+                                         &coefficient);
+
+      FEValues<dim> fe_values(*fe,
+                              quad,
+                              update_hessians | update_JxW_values |
+                                update_quadrature_points);
+
+      std::vector<double> local_laplacians(quad.size());
+
+
+      double residual_L2_norm = 0;
+
+      unsigned int cell_index = 0;
+      for (const auto &cell : dof_handler.active_cell_iterators())
+        {
+          fe_values.reinit(cell);
+
+          fe_values.get_function_laplacians(solution, local_laplacians);
+          residual_L2_norm = 0;
+          for (const auto q_index : fe_values.quadrature_point_indices())
+            {
+              const auto arg =
+                (local_laplacians[q_index] +
+                 forcing_term.value(fe_values.quadrature_point(q_index)));
+              residual_L2_norm += arg * arg * fe_values.JxW(q_index);
+            }
+          error_per_cell[cell_index] +=
+            cell->diameter() * std::sqrt(residual_L2_norm);
+
+          ++cell_index;
+        }
+    }
+  else
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+}
+
+
+
+template <int dim>
+void
+Poisson<dim>::mark()
+{
+  if (marking_strategy == "global")
+    {
+      for (const auto &cell : triangulation.active_cell_iterators())
+        cell->set_refine_flag();
+    }
+  else if (marking_strategy == "fixed_fraction")
+    {
+      GridRefinement::refine_and_coarsen_fixed_fraction(
+        triangulation,
+        error_per_cell,
+        coarsening_and_refinement_factors.second,
+        coarsening_and_refinement_factors.first);
+    }
+  else if (marking_strategy == "fixed_number")
+    {
+      GridRefinement::refine_and_coarsen_fixed_number(
+        triangulation,
+        error_per_cell,
+        coarsening_and_refinement_factors.second,
+        coarsening_and_refinement_factors.first);
+    }
+  else
+    {
+      Assert(false, ExcInternalError());
+    }
+}
 
 template <int dim>
 void
@@ -246,6 +400,18 @@ Poisson<dim>::output_results(const unsigned cycle) const
 
 template <int dim>
 void
+Poisson<dim>::compute_error()
+{
+  auto global_estimator = error_per_cell.l2_norm();
+  error_table.add_extra_column("estimator", [global_estimator]() {
+    return global_estimator;
+  });
+  error_table.error_from_exact(dof_handler, solution, exact_solution);
+}
+
+
+template <int dim>
+void
 Poisson<dim>::run()
 {
   make_grid();
@@ -254,10 +420,14 @@ Poisson<dim>::run()
       setup_system();
       assemble_system();
       solve();
-      error_table.error_from_exact(dof_handler, solution, exact_solution);
+      estimate();
+      compute_error();
       output_results(cycle);
       if (cycle < n_refinement_cycles - 1)
-        refine_grid();
+        {
+          mark();
+          refine_grid();
+        }
     }
   error_table.output_table(std::cout);
 }
